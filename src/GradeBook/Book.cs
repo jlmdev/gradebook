6e963a519bf693fd308c9293b223d2353076e69e
Josh Mann <jlmann72@gmail.com>
2021-03-04 06:36:13 -0500

    changed loops

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -30,12 +30,14 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+            for (var index = 0; index < grades.Count; index++)
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
+                index++;
             }
+
             result.Average /= grades.Count;
 
             return result;
