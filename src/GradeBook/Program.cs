7013c91f6a5b41ed633047a943b6d1947001b03d
Josh Mann <jlmann72@gmail.com>
2021-03-04 08:56:39 -0500

    properties

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,14 +8,40 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Josh's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            while (true)
+            {
+                System.Console.WriteLine("Please enter a grade or press Q to stop");
+                var gradeEntry = Console.ReadLine();
+                if (gradeEntry == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(gradeEntry);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    System.Console.WriteLine("**");
+                }
+            }
 
             var stats = book.GetStatistics();
+            System.Console.WriteLine($"For the book named {book.Name}");
             System.Console.WriteLine($"The lowest grade is {stats.Low}");
             System.Console.WriteLine($"The highest grade is {stats.High}");
             System.Console.WriteLine($"The average grade is {stats.Average:N1}");
+            System.Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
